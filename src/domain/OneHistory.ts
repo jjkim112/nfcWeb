ab51489cfb04eb7ce2f41bbc9ffc1d4244f5c386
jjkim112 <jjkim112@naver.com>
2023-09-05 20:45:08 +0900

    deviceHash

--- src/domain/OneHistory.ts
+++ src/domain/OneHistory.ts
@@ -4,40 +4,44 @@ export class OneHistory {
   lat: number | null;
   lon: number | null;
   updateTime: Date | null;
-  deviceId: string | null;
+  deviceInfo: string | null;
+  deviceHash: string | null;
 
   constructor(
     lat: number | null,
     lon: number | null,
     updateTime: Date | null,
-    deviceId: string | null,
+    deviceInfo: string | null,
+    deviceHash: string | null,
   ) {
     this.lat = lat;
     this.lon = lon;
     this.updateTime = updateTime;
-    this.deviceId = deviceId;
+    this.deviceInfo = deviceInfo;
+    this.deviceHash = deviceHash;
   }
   static fromData(data: any): OneHistory | null {
     try {
       return new OneHistory(
-        data['lat'],
-        data['lon'],
+        data['coordinate']['lat'] ?? 0,
+        data['coordinate']['lon'] ?? 0,
         (data['updateTime'] as Timestamp).toDate(),
-        data['deviceId'],
+        data['deviceInfo'],
+        data['deviceHash'],
       );
     } catch (e) {
       console.log(`[OneHistory.fromData] ${e}`);
-      return new OneHistory(0, 0, new Date(0), '');
+      return new OneHistory(0, 0, new Date(0), '', '');
     }
   }
   // Firestore data converter
 
   toMapForFireBase(): any {
     return {
-      lat: this.lat,
-      lon: this.lon,
+      coordinate: { lat: this.lat, lon: this.lon },
       updateTime: this.updateTime,
-      deviceId: this.deviceId,
+      deviceInfo: this.deviceInfo,
+      deviceHash: this.deviceHash,
     };
   }
 }
--- src/pages/layout/TestPage.tsx
+++ src/pages/layout/TestPage.tsx
@@ -6,13 +6,19 @@ import {
 import { OneHistory } from 'domain/OneHistory';
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { deviceDetect } from 'mobile-device-detect';
+import {
+  deviceDetect,
+  getUA,
+  mobileVendor,
+  mobileModel,
+} from 'mobile-device-detect';
 import { useSearchParams } from 'react-router-dom';
+import SHA256 from 'crypto-js/sha256';
 
 export default function TestPage() {
   const [serachParams, setSerachParams] = useSearchParams();
   const [testBool, setTestBool] = useState(false);
-  const [testInfo, setTestInfo] = useState();
+  const [testInfo, setTestInfo] = useState(null);
 
   function getPosition(): Promise<any> {
     return new Promise((resolve, reject) =>
@@ -26,12 +32,15 @@ export default function TestPage() {
     try {
       const data = await getPosition();
       //deviceid가져오는것
-      const deviceData = await deviceDetect();
+      const deviceUaData = getUA;
+      const deviceHash = SHA256(deviceUaData).toString();
+      const deviceInfo = `${mobileModel}-${mobileVendor}`;
       return {
         lat: data.coords.latitude,
         lon: data.coords.longitude,
         updateTime: Number(data.timestamp),
-        deviceId: deviceData,
+        deviceInfo: deviceInfo,
+        deviceHash: deviceHash,
       };
     } catch (error) {
       //TODO 에러떳을때 처음인 경우 위치수락하시겠습니까? 뜨도록 ->이거불가능해 안해
@@ -53,37 +62,32 @@ export default function TestPage() {
     //2-1.id & categoryId 받아오기
 
     //2-2.위치 & 시간 정보 받아와서 맵핑하기
+
     const mapData = await getHistoryMapDataToBeUpdate();
-    console.log(mapData?.deviceId);
-    setTestInfo(mapData?.deviceId);
+    console.log(mapData);
+
     //3.updateHistory 라는 함수를 만들것이다. 인풋은 id,type<경로용> 맵화된 시간,시간 위치정보<데이터용>
     // bool isSuccess = await updateHistory(id, type, mapData);
-    //http://localhost:3000/test?id=4981d82551390&typeId=aswaswasw
-    //id=4981d82551390 typeId=aswaswasw
+
     //3-1-a.데이터를 통채로 불러와서 history추가한뒤 set하기
 
     const newHistoryData: OneHistory = new OneHistory(
       mapData?.lat,
       mapData?.lon,
       new Date(mapData?.updateTime ?? 0),
-      mapData?.deviceId ?? 'No device info',
+      mapData?.deviceInfo ?? 'No device info',
+      mapData?.deviceHash ?? 'No device Hash',
     );
-
-    // const isSuccess = await testSetData(
-    //   'aswaswasw',
-    //   '4981d82551390',
-    //   newHistoryData,
-    // );
 
     //3-1-b.update하기
     //////////////////////////////////////////////////////////
-    // const isSuccess = await testUpdateData(
-    //   'nO4H4EPp61LWam9Nsd8t',
-    //   'ssm3-020f268b-a64e-45b3',
-    //   newHistoryData,
-    // );
+    const isSuccess = await testUpdateData(
+      'nO4H4EPp61LWam9Nsd8t',
+      'ssm3-020f268b-a64e-45b3',
+      newHistoryData,
+    );
 
-    // setTestBool(isSuccess);
+    setTestBool(isSuccess);
     ////////////////////////////////////////////////////////
     //3-2.firebase에서 제공하는 arrayUnion 활용하기
 
